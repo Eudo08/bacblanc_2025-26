3ffe4b963b85c22cf40189313d6588bc60de0e22
Eudo08 <e2khovrine92@gmail.com>
2025-09-24 09:25:04 +0200

    Add Gateau_anniversaire subclass and improve output
    Introduces a Gateau_anniversaire class inheriting from Gateau, with additional attributes and methods for birthday cakes. Also improves the output formatting in presenter_gateau and cuire methods, and corrects method calls in the main script.

--- class.py
+++ class.py
@@ -23,13 +23,33 @@ class Gateau  () :
         self.temps_cuisson = temps_cuisson
 
     def presenter_gateau (self) :
-        texte = f"\Je suis le {self.nom} et je suis constitué d'{self.ingrédients}." + f"Le gateau cuit à 180°C pendant {self.temps_cuisson}."
+        texte = f"\nJe suis le {self.nom}.\n" \
+            + f"je suis constitué d'{self.ingrédients}.\n" \
+            + f"Le gateau cuit à 180°C pendant {self.temps_cuisson}.\n"
         print (texte)
    
     def cuire (self) :
         texte = f"\nCuisson spéciale pour le {self.nom} :" \
-            + f" - Enfourner à 180°C pendant {self.temps_cuisson} minutes"
+            + f" - Enfourner à 180°C pendant {self.temps_cuisson} minutes\n"
         
 my_cake = Gateau ("marbré", ["beurre", "oeufs", "sucre", "chocolat"], 35)
-my_cake.presenter ()
+my_cake.presenter_gateau ()
 my_cake.cuire ()
+
+class Gateau_anniversaire (Gateau):
+    def __init__ (self, nom, ingrédients, temps_cuisson, nbre_bougies) :
+        super ().__init__ (nom, ingrédients, temps_cuisson)
+        self.nbre_bougie = nbre_bougies
+    
+    def presenter_bis (self) :
+        self.presenter_gateau ()
+        print ("JOYEUX ANNIVERSAIRE !!")
+
+    def cuire (self, alpha) :
+        texte = f"\nCuisson spéciale pour le {self.nom} :" \
+            + f" - Enfourner à 200°C pendant {self.temps_cuisson} minutes" + f"le paramètre est {alpha}"
+        print (texte)
+my_birth_cake = Gateau_anniversaire ("tarte à l'orange", \
+                                     ["farine", "oeufs", "sucre", "orange"], \
+                                        50, 55)
+my_birth_cake.presenter_bis()
