ed44d3b4b8d8aa45b5099c3b091858d374a27220
Eudo08 <e2khovrine92@gmail.com>
2025-11-07 15:02:34 +0100

    Add tree traversal and binary search functions
    Implemented recursive functions for prefix, infix, and suffix traversals of binary trees. Added a function to check if a tree is a binary search tree. Updated make_node to use default None for left and right children and standardized key names.

--- tp_arbres_binaires__recursivite.py
+++ tp_arbres_binaires__recursivite.py
@@ -1,7 +1,7 @@
 # On commence par dérire un arbre à l'aide de dictionnaires python.
 # Pour cela on utilise la fonction node définie comme ci-après:
-def make_node (root, left, right):
-    return {"r": root, "left": left, "right" : right}
+def make_node (root, left=None, right=None):
+    return {"root": root, "left": left, "right" : right}
 
 a = make_node (0)
 b = make_node (4)
@@ -19,24 +19,89 @@ A = make_node (15, h, k)
 
 # Ecrire une fonction récursive qui calcule la hauteur d'un arbre binaire
 
-def height (tree) :
-    if tree == {} or tree == None:
-        return "C'EST VIDEEEEEEEEE"
-    else :
-        return 1 + max (height(tree ["left"]), \
-                               height (tree ["right"]))
+# def height (tree) :
+#     if tree == {} or tree == None:
+#         return "C'EST VIDEEEEEEEEE"
+#     else :
+#         return 1 + max (height(tree ["left"]), \
+#                                height (tree ["right"]))
 
-print (height (A))
+# print (height (A))
 
 
 # Ecrire une fonction récursive qui décrit le parcours PREFIXE d'un arbre binaire
 
+def prefixe (tree) :
+    if tree == {} or tree == None :
+        return []
+    else :
+        return [tree["root"]] \
+            + prefixe (tree["left"]) \
+            + prefixe (tree["right"])
+
+print (prefixe(A))
+
+
+
+
+
+
+
 # Ecrire une fonction récursive qui décrit le parcours INFIXE d'un arbre binaire
+def infixe (tree) :
+    if tree == {} or tree == None :
+        return []
+    else :
+        return infixe(tree["left"]) \
+            + [tree["root"]] \
+            + infixe (tree["right"])
+
+print (infixe(A))
 
 # Ecrire une fonction récursive qui décrit le parcours SUFFIXE d'un arbre binaire
+def suffixe (tree) :
+    if tree == {} or tree == None :
+        return []
+    else :
+        return suffixe(tree["left"]) \
+            + suffixe (tree["right"]) \
+            + [tree["root"]]
+ 
+            
+print (suffixe(A))
 
 # Ecrire une fonction récursive qui décrit le parcours EN LARGEUR d'un arbre binaire
 
 # Ecrire une fonction récursive qui dit si une valeur est dans un arbre binaire
+# def value_in_binary_tree (value, tree) :
+#     if tree == None :
+#         return False
+#     else :
+#         return tree ["root"] == value \
+#             or value_in_binary_tree (value, tree ["left"]) \
+#             or value_in_binary_tree (value,tree["right"])
 
+# print (value_in_binary_tree(2, A))        
 # Ecrire une fonction récursive qui dit si un arbre binaire est un de recherche
+
+def binary_search (tree, check_roots) :
+    if tree == None :
+        return True
+    elif tree ["left"] == None and tree ["right"] == None  :
+        return True
+    else :
+        if tree ["left"] == None and tree ["right"] != None :
+            check_roots = tree ["root"] <= tree ["right"]["root"]
+        elif tree ["left"] != None and tree ["right"] == None :
+            check_roots = tree ["left"]["root"] <= tree ["root"]
+        else :
+            check_roots = \
+                    tree ["left"]["root"] \
+                    <= tree ["root"] \
+                    <= tree ["right"]["root"]
+        return check_roots \
+            and binary_search (tree ["left"], check_roots) \
+            and binary_search (tree ["left"], check_roots)
+
+
+print (binary_search(A, True))
